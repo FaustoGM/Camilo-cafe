440d84d1518ab7cabdd36799c821c274b6eadddb
Fausto <faustogiordanom@gmail.com>
2025-04-04 12:58:16 -0300

    Fix definitivo: FlutterLoader configurado correctamente

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "445710741"
+    serviceWorkerVersion: "1267297228"
   }
 });
